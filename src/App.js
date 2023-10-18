3c4e59d24c97323583dc0d0e808e7111d0142900
Amirhossein Shahbabaee <amirhossein.shahbabaee@citynetwork.eu>
2023-10-18 19:27:37 +0200

    Enter values

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import "./App.css";
 import Board from "./Board";
 import Keyboard from "./Keyboard";
@@ -8,17 +8,44 @@ const ROWS = 6;
 
 function App() {
   const [solution, setSolution] = useState("");
-  const [guesses, setGuesses] = useState(new Array(ROWS).fill("abc"));
+  const [guesses, setGuesses] = useState(new Array(ROWS).fill(""));
   const [currentRow, setCurrentRow] = useState(0);
+  const [currentWord, setCurrentWord] = useState("");
   const [letters, setLetters] = useState("");
 
   useEffect(() => {
     setSolution(words[Math.floor(Math.random() * words.length)]);
   }, []);
 
+  const handleKeyDown = useCallback(
+    (e) => {
+      const { key, keyCode } = e;
+      if (currentWord.length === 5) {
+        if (keyCode !== 13 && keyCode !== 8) return;
+      }
+
+      if (keyCode >= 65 && keyCode <= 90) {
+        setCurrentWord((currentRow) => currentRow + key);
+      }
+    },
+    [currentWord]
+  );
+
+  useEffect(() => {
+    document.addEventListener("keydown", handleKeyDown);
+    return () => {
+      document.removeEventListener("keydown", handleKeyDown);
+    };
+  }, [handleKeyDown]);
+
   return (
     <div className="App">
-      <Board guesses={guesses} />
+      {currentWord}
+      <Board
+        guesses={guesses}
+        currentWord={currentWord}
+        currentRow={currentRow}
+      />
       <Keyboard letters={[]} />
     </div>
   );
--- src/Board.js
+++ src/Board.js
@@ -1,10 +1,12 @@
-const Board = ({ guesses }) => (
+const Board = ({ guesses, currentWord, currentRow }) => (
   <div className="board">
     {guesses.map((row, rowIdx) => (
       <div key={rowIdx} className="board__row">
         {new Array(5).fill("").map((cell, cellIdx) => (
           <div className="board__cell" key={cellIdx}>
-            {row[cellIdx] ?? ""}
+            {currentRow >= rowIdx
+              ? currentWord[cellIdx] ?? ""
+              : row[cellIdx] ?? ""}
           </div>
         ))}
       </div>
